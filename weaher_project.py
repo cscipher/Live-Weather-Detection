8d1b2743337094c6859c89b75024e36efa1ba801
Harsh Soni <57137427+xCiPH3R@users.noreply.github.com>
2020-03-25 10:51:50 +0530

    Update weaher_project.py

--- weaher_project.py
+++ weaher_project.py
@@ -9,7 +9,7 @@ with sr.Microphone() as source:
     playsound('call.mp3',True)
     print("Tell the Location : ",end='')
     audio=r.listen(source)
-    print(audio)
+    
     try:
         city=r.recognize_google(audio)
         print(city+'\n')
@@ -20,7 +20,7 @@ weather_url = "'http://api.openweathermap.org/data/2.5/weather"
 ap_id = "9e820fd2ed2534c9be417de07eb35ea6"
 
 #temp = requests.put(weather_url,data={"q":city,"appid":ap_id})
-temp = requests.get('http://api.openweathermap.org/data/2.5/weather?q={0}&appid=9e820fd2ed2534c9be417de07eb35ea6'.format(city)).json()
+temp = requests.get('http://api.openweathermap.org/data/2.5/weather?q={0}&appid={ap_id}'.format(city, ap_id)).json()
 
 feels = temp['weather'][0]['description']
 temprature = temp['main']['temp']
@@ -33,4 +33,5 @@ language = 'en-us'
 obj = gTTS(text=weather, lang=language, slow=False)
 
 obj.save("welcome.mp3")
+print(weather)
 playsound('Welcome.mp3',True)
