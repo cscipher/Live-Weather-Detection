b560a7d2c9cc84e0cd99d2438c5c0002c31fdc2c
Harsh Soni <57137427+xCiPH3R@users.noreply.github.com>
2020-03-25 10:04:42 +0530

    Weather.py

---
+++ weaher_project.py
@@ -0,0 +1,36 @@
+from gtts import gTTS
+import os, requests
+from playsound import playsound
+import speech_recognition as sr 
+
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+    playsound('r.mp3',True)
+    #print("Say City Name : ",end='')
+    audio=r.listen(source)
+    print(audio)
+    try:
+        city=r.recognize_google(audio)
+        print(city+'\n')
+    except:
+        print("NOT PROPERLY AUDIBLE")
+
+weather_url = "'http://api.openweathermap.org/data/2.5/weather"
+ap_id = "9e820fd2ed2534c9be417de07eb35ea6"
+
+#temp = requests.put(weather_url,data={"q":city,"appid":ap_id})
+temp = requests.get('http://api.openweathermap.org/data/2.5/weather?q={0}&appid=9e820fd2ed2534c9be417de07eb35ea6'.format(city)).json()
+
+feels = temp['weather'][0]['description']
+temprature = temp['main']['temp']
+temprature_new = 'Current Temperature is '+str(round((temprature-273.16), 1))+' degree celsius, '
+feels_new = "and, outside, it is "+feels
+
+weather = temprature_new+feels_new
+
+language = 'en-us'
+obj = gTTS(text=weather, lang=language, slow=False)
+
+obj.save("welcome.mp3")
+playsound('Welcome.mp3',True)
